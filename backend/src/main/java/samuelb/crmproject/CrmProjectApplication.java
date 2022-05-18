2853e4ad34162295517f62d206c2c9890faab39f
Sam-B-Hub <64763981+Sam-B-Hub@users.noreply.github.com>
2022-05-18 17:14:42 +1000

    generated api documentation with swagger

--- backend/src/main/java/samuelb/crmproject/CrmProjectApplication.java
+++ backend/src/main/java/samuelb/crmproject/CrmProjectApplication.java
@@ -3,7 +3,11 @@ package samuelb.crmproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 @SpringBootApplication
+@EnableSwagger2
 public class CrmProjectApplication {
 
 	public static void main(String[] args) {
